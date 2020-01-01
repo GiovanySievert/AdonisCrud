c6a7648fd9c0c5803f5c947bfe59a4744c82e512
Giovany Sievert <giovanysievert@gmail.com>
2020-01-01 14:51:44 -0300

    first commit

---
+++ start/routes.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+
+const Route = use("Route");
+
+Route.post("users", "UserController.store");
+Route.post("session", "SessionController.store");
+
+Route.post("password", "ForgotPasswordController.store");
+Route.put("password", "ForgotPasswordController.update");
+
+Route.post("file", "FileController.store");
